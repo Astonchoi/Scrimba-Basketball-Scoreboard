0977ad8251adcb9838435e0ca82607299bf902e1
Aston0523 <choilamchun2003@gmail.com>
2026-02-04 17:04:09 +0800

    add button: undo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let homeScoreEl = document.getElementById('home-score')
 let guestScoreEl = document.getElementById('guest-score')
+let homeScoreHistory = [0]
+let guestScoreHistory = [0]
+
 
 const defaults = {
   spread: 360,
@@ -31,15 +34,30 @@ function plusButton(btn){
     // update the text of scoreEl
     if (place === 'home' && Number(homeScoreEl.textContent) < 10 && Number(guestScoreEl.textContent) < 10){
         homeScoreEl.textContent = Number(homeScoreEl.textContent) + score
+        homeScoreHistory.push(Number(homeScoreEl.textContent))
         if (Number(homeScoreEl.textContent) >= 10){
             shoot()
         }
     }
     else if(place === 'guest' && Number(guestScoreEl.textContent) < 10 && Number(homeScoreEl.textContent) < 10){
         guestScoreEl.textContent = Number(guestScoreEl.textContent) + score
+        guestScoreHistory.push(Number(guestScoreEl.textContent))
         if (Number(guestScoreEl.textContent) >= 10){
             shoot()
         }
+    }
+}
+
+function undo(btn){
+    let buttonId = btn.id
+    let place = buttonId.split('-')[0]
+    if (place === 'home' && homeScoreHistory.length > 1){
+        homeScoreHistory.pop()
+        homeScoreEl.textContent = homeScoreHistory[homeScoreHistory.length - 1]
+    }
+    if (place === 'guest' && guestScoreHistory.length > 1){
+        guestScoreHistory.pop()
+        guestScoreEl.textContent = guestScoreHistory[guestScoreHistory.length - 1]
     }
 }
 
